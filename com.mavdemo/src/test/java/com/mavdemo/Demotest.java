102a22b11daedc0ddaf23bcb199914d0e3a623e5
Giti <Gitimaya@192.168.1.4>
2017-06-18 23:38:49 +0530

    added println

--- com.mavdemo/src/test/java/com/mavdemo/Demotest.java
+++ com.mavdemo/src/test/java/com/mavdemo/Demotest.java
@@ -6,5 +6,6 @@ public class Demotest {
 	@Test
 	public void testmav(){
 		System.out.println("Working with Maven ? It's great .");
+		System.out.println("Also linked with Jenkins. Rite Giti ?");
 	}
 }
