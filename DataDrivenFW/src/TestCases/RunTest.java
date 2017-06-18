0bdfa774f72b8511b5a03e71f0a18a204ea50785
Giti <Gitimaya@192.168.1.4>
2017-06-18 17:11:32 +0530

    print updated

--- DataDrivenFW/src/TestCases/RunTest.java
+++ DataDrivenFW/src/TestCases/RunTest.java
@@ -26,7 +26,7 @@ public class RunTest {
 	  String un1 = pro.getProperty("user1");
 	  String pw1 = pro.getProperty("pwd1");
 	  //Object Repository***********************
-	  System.out.println("For GIT");
+	  System.out.println("For GIT n jenkins");
 	  
 	  //Select Specific Browser
 	  Browsers.selectBrowser("chrome");
